f0f693e36127061048fa40fc8d3b2b5c754c1908
brotzky <brotzky@gmail.com>
2022-10-26 12:34:10 -0700

    Add RSS Feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,58 @@
+import fs from "fs";
+import { Feed as RSSFeed } from "feed";
 import Feed from "../components/Feed";
 import SEO from "../components/SEO";
-import { getArticles } from "../db/articles";
+import { IArticle, getArticles } from "../db/articles";
 import { getTags } from "../db/tags";
+
+async function generateFeed(articles: IArticle[]) {
+  const siteURL = process.env.VERCEL_URL;
+
+  const date = new Date();
+  const feed = new RSSFeed({
+    title: "bleeding edge",
+    description: "bleeding edge is a feed of noteworthy developments in AI.n",
+    id: siteURL,
+    link: siteURL,
+    image: `${siteURL}/favicon/favicon-grey.svg`,
+    favicon: `${siteURL}/favicon/favicon-grey.svg`,
+    copyright: `All rights reserved ${date.getFullYear()}, Jatin Sharma`,
+    updated: date,
+    generator: "Feed for bleedingedge.ai",
+    feedLinks: {
+      rss2: `${siteURL}/rss/feed.xml`, // xml format
+      json: `${siteURL}/rss/feed.json`, // json fromat
+    },
+    author: {
+      name: "Lachy Groom",
+      email: "lachy@bleedingedge.ai",
+      link: "https://twitter.com/bleedingedge.ao",
+    },
+  });
+
+  articles.forEach((article) => {
+    feed.addItem({
+      title: article.title,
+      id: article.url,
+      link: article.url,
+      description: article.blurb,
+      content: article.blurb,
+      date: new Date(article.posted_at),
+    });
+  });
+
+  fs.mkdirSync("./public/rss", { recursive: true });
+  fs.writeFileSync("./public/rss/feed.xml", feed.rss2());
+  fs.writeFileSync("./public/rss/feed.json", feed.json1());
+}
 
 export async function getStaticProps() {
   const articlesRequest = getArticles({ tags: [] });
   const tagsRequest = getTags();
 
   const [articles, tags] = await Promise.all([articlesRequest, tagsRequest]);
+
+  await generateFeed(articles);
 
   return {
     props: {
