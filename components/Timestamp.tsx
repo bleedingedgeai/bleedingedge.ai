00fcdc933344dce8d62e309ff8760d4abb987181
brotzky <brotzky@gmail.com>
2022-10-30 12:04:29 -0700

    Last week to Past week

--- components/Timestamp.tsx
+++ components/Timestamp.tsx
@@ -20,7 +20,7 @@ const formatDateString = (dateString: string) => {
     case yesterday:
       return `Yesterday, ${shortDate}`;
     case lastWeek:
-      return "Last week";
+      return "Past week";
     default:
       return new Intl.DateTimeFormat("en", {
         year: "numeric",
